a5d24ec9fa51ffce9e44fb7927e1b5f102b7d8d3
Marcin Nowakowski <marcin.nowakowski17@protonmail.com>
2020-12-01 20:46:26 +0100

    Initial commit

---
+++ Assets/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+    [SerializeField] GameObject _patrolPointPrefab;
+    public List<GameObject> _patrolPoints;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    Debug.Log("trfienie");
+                    _patrolPoints.Add(Instantiate(_patrolPointPrefab, new Vector3(hit.point.x, 0, hit.point.z), Quaternion.identity));
+                }
+                if (hit.collider.CompareTag("PatrolPoint"))
+                {
+                    _patrolPoints.Remove(hit.collider.gameObject);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    public List<GameObject> GetPatrolPoints()
+    {
+        return _patrolPoints;
+    }
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] Patrol _patrol;
+    [SerializeField] NavMeshAgent _player;
+    public List<GameObject> _patrolPoints;
+    public Vector3 _currentDestination;
+    public int _currentDestinationIndex;
+
+    private void Update()
+    {
+        _patrolPoints = _patrol.GetPatrolPoints();
+
+        if (_patrolPoints.Count > 0)
+        {
+            if (_currentDestination == null)
+            {
+                _currentDestinationIndex = 0;
+                _currentDestination = _patrolPoints[0].transform.position;
+            }
+
+            if (Vector3.Distance(transform.position, _currentDestination) < 0.2f)
+            {
+                Debug.Log("cel osiagniety: " + _currentDestinationIndex);
+                _currentDestinationIndex++;
+                if ((_currentDestinationIndex) >= _patrolPoints.Count)
+                {
+                    _currentDestinationIndex = 0;
+                    _currentDestination = _patrolPoints[_currentDestinationIndex].transform.position;
+                }
+                else
+                    _currentDestination = _patrolPoints[_currentDestinationIndex].transform.position;
+
+                /*
+                if (_currentDestinationIndex == (_patrolPoints.Count - 1))
+                {
+                    _currentDestinationIndex = 0;
+                    _currentDestination = _patrolPoints[0].transform.position;
+                }
+                else
+                {
+
+                        _currentDestinationIndex++;
+                        if ((_currentDestinationIndex + 1) >= _patrolPoints.Count)
+                        {
+                            _currentDestinationIndex = 0;
+                            _currentDestination = _patrolPoints[_currentDestinationIndex].transform.position;
+                        }
+                        else
+                            _currentDestination = _patrolPoints[_currentDestinationIndex].transform.position;
+                    
+                }*/
+            }
+
+            _player.SetDestination(_currentDestination);
+        }
+
+    }
+}
